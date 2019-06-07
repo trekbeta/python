a2f0f9715c8589b01dc39249985e89b1af0fe97b
trekbeta <zinho.valtinho@gmail.com>
2019-06-07 14:13:26 -0300

    Update urls.py

--- urls.py
+++ urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 
 from . import views
-
 urlpatterns = [
     path('helloworld/', views.helloWorld),
-    path('', views.taskList, name='taks-list'),
+    path('', views.taskList, name='Task-list'),
+    path('yourname/<str:name>', views.yourName, name='your-name'),
 ]
